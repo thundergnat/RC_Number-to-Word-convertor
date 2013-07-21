f0bc1e0ec14dd16b62535173ecbdb0a48dc0347d
ScalaPino <vdenberg+ScalaPino@gmail.com>
2013-07-21 15:17:08 +0200

    Notice this commit is earlier than the www publication.
    Tested parallel version.
    Published on http://rosettacode.org/wiki/Number_names#Scala

--- src/main/scala/org/rosettacode/numbernames/SpellNumber.scala
+++ src/main/scala/org/rosettacode/numbernames/SpellNumber.scala
@@ -1,88 +1,111 @@
 package org.rosettacode
 package numbernames
 
-object SpellNumber {
+import annotation.tailrec
+import collection.parallel.ParSeq
+
+/** Spells a number longhand
+ *
+ *  The implementation goes up to 10<sup>69</sup>-1 and also supports negative and zero inputs.
+ *
+ *  @example longhand( 1234 )  // results in: "one thousand two hundred thirty-four".
+ */
+object SpellNumber extends App {
+
   /** Spells a number longhand
    *
-   *  @example longhand( 1234 )  // results in: one thousand two hundred thirty-four
+   *  The solution is recursive by the triplets of decimal numbers.
+   *  @param v				the numeric value to be converted
+   *  @param showAnd		flag the output extra and in output, default off
+   *  @param zeroString		the word for 0, default to "zero"
+   *  @param showHyphen		shows the hyphen e.g. twenty-four, default is on
+   *  @return				the numeric value in words
    */
-  def longhand(v: Long, showAnd: Boolean = true, showHyphen: Boolean = true): String = {
-    // Based on a solution of Rex Kerr's
+  def longhand(v: BigInt,
+               zeroString: String = "zero",
+               showAnd: Boolean = false,
+               showHyphen: Boolean = true): String = {
 
     val onesAndTeens = {
-      val a1 = Array[String]("")
-      val a2 = "one two three four five six seven eight nine ten eleven twelve".split(' ').map(_ + " ")
-      val a3 = "thir four fif six seven eigh nine".split(' ').map(_ + "teen ")
-      a1 ++ a2 ++ a3
+      val a1 = "one two three four five six seven eight nine ten eleven twelve".split(' ').map(_ + " ")
+      val a2 = "thir four fif six seven eigh nine".split(' ').map(_ + "teen ")
+      (Array[String]("") ++ a1 ++ a2).par
     }
 
-    val tens = {
-      val a1 = Array[String]("", "")
-      val a2 = "twen thir for fif six seven eigh nine".split(' ').map(_ + "ty")
-      a1 ++ a2
-    }
+    val tens = (Array[String]("", "") ++ ("twen thir for fif six seven eigh nine".split(' ')).
+      map(_ + "ty")).par
 
-    val hundredString = "hundred "
-    val andString = if (showAnd) "and " else ""
-    val hyphenString = if (showHyphen) "-" else " "
+    def hundredString = "hundred "
+    def condAndString = if (showAnd) "and " else ""
+    def condHyphenString = if (showHyphen) "-" else " "
 
     val powersOfThousands = {
-      val a1 = Array[String]("", "thousand ")
-      val a2 = "m b tr quadr quint sext sept oct non dec".split(' ').map(_ + "illion ")
-      val a3 = "un duo tre quattuor quin sex septen octo novem ".split(' ').map(_ + "decillion ")
-      val a4 = "vigint cent".split(' ').map(_ + "illion ")
-      a1 ++ a2 ++ a3 ++ a4
+      val p1 = "m b tr quadr quint sext sept oct non dec".split(' ').map(_ + "illion ").par
+      val p2 = "un duo tre quattuor quin sex septen octo novem ".split(' ').map(_ + "decillion ").par
+      val p3 = "vigint cent".split(' ').map(_ + "illion ") ++ Array("overflow trap").par
+      (Array("", "thousand ") ++ p1 ++ p2 ++ p3).par
     }
 
     // 234 becomes "two hundred [and] thirty-four"
-    def composeScale(v: String, isFirst: Boolean): String = {
+    def tripletGroup(nnn: String, isLSDgroup: Boolean, s: String): String = {
+      if (nnn != "000") {
+        val eval = (nnn.par.map(_.toString.toInt).reverse zip ParSeq('units, 'tens, 'hundreds)).reverse
 
-      val e1 = (v.map(_.toString.toInt).reverse zip List("", "-", "hundred")).reverse
-
-      e1.map {
-        case (d, "hundred") if d > 0 => onesAndTeens(d) + hundredString + andString
-        case (d, "-") if d > 1 && !e1.contains((0, "")) && isFirst => tens(d) + hyphenString
-        case (d, "-") if d > 1 && !e1.contains((0, "")) => tens(d) + " "
-        case (d, "-") if d > 1 => tens(d)
-        case (d, "") if e1.contains((1, "-")) => onesAndTeens(d + 10)
-        case (d, "") => onesAndTeens(d)
-        case _ => ""
-      }.mkString
+        eval.map {
+          case (d, 'units) if eval.seq.contains((1, 'tens)) => onesAndTeens(d + 10)
+          case (d, 'units) if (isLSDgroup && nnn == "0") => zeroString
+          case (d, 'units) => onesAndTeens(d)
+          case (d, 'hundreds) if d > 0 => onesAndTeens(d) + hundredString + condAndString
+          case (d, 'tens) if d > 1 && eval.seq.contains((0, 'units)) => tens(d)
+          case (d, 'tens) if d > 1 => tens(d) +
+            ( /*if (isLSDgroup) discutable*/ condHyphenString /* else " " */ )
+          case _ => ""
+        }.mkString + s
+      } else ""
     }
 
-    def compose(s: String): String = {
+    def compose(n: BigInt): String = {
+      // "1234" becomes List((1,"thousand"), (234, ""))
+      val decGroups = n.toString.reverse.grouped(3).map(_.reverse).toSeq.par // Group into powers of thousands
+        .zip(powersOfThousands) //						// Name the powers of Thousands
+        .reverse //										// Put it back to most-significant first
 
-      // "1234" becomes List(((1,false),"thousand"), ((234,true),""))
-      val thousands = {
-        def first(s: String) = (true) #:: Stream.continually(false) // First element true others false
-        val e1 = s.reverse.grouped(3).map(_.reverse).toList // Group into powers of thousands
-        val e2 = e1 zip first(s) // Mark the first element
-        val e3 = e2 zip powersOfThousands // Name the powers of Thousands
-        e3.reverse // Put it back to most-significant first
-      }
+      if (decGroups.size < 24) // Detect overflow trap
+      // Send per group sections to composeScale
+      {
+        @tailrec
+        def iter(elems: Seq[(String, String)], acc: String): String = {
+          elems match {
+            case (group, powers) :: tail => {
+              iter(tail, acc + tripletGroup(group, tail == Nil, powers))
+            }
+            case _ => acc
+          }
+        }
 
-      thousands.map { case ((v, isFirst), s) => composeScale(v, isFirst) + s }.mkString.trim
+        iter(decGroups.toList, "").mkString.trim
+      } else "###.overflow.###"
+    } // def compose(…
+
+    // Here starts def longhand(…
+    if (v < 0) "minus " + compose(-v) else compose(v)
+  }
+
+  // Main entry A little test...
+  { // Anonymous ordered list as test set
+    def testVal1 = BigInt("1" * 69)
+    def testVal9 = BigInt(10).pow(69) - 1
+
+    @tailrec // Series generator of 9, 98, 987, 9876 …
+    def inner(counter: Int, elem: BigInt, testList: ParSeq[BigInt]): ParSeq[BigInt] = {
+      if (counter < 20)
+        inner(counter + 1, elem * 10 + (9 - (counter % 10)), testList ++ ParSeq(elem))
+      else testList.par
     }
-
-    if (v < 0) "minus " + compose((-v).toString) else compose(v.toString)
-  }
-
-  // A little test...		1000000000 and 0 fails!
-  def main(args: Array[String]): Unit = {
-    //TODO: pure powers of 10 fails
-    //TODO: 0 fails
-    //TODO: showAnd fails at 100
-
-    { // Anonymous ordered list with test values
-      def testVal1 = 1000000000000000000L
-      def testVal9 = 999999999999999999L
-      def inner(counter: Int, acc: Long, testList: List[Long]): List[Long] = {
-        if (counter < 20)
-          inner(counter + 1, acc * 10 + (9 - (counter % 10)), testList ++ List(acc))
-        else testList
-      }
-      inner(0, 0L, List(-testVal1, 12, 19, 100, testVal1, testVal9, Long.MaxValue - 13))
-    }.sorted.
-      foreach(num => println(f"""$num%,26d is ${longhand(num)}"""))
-  }
-} // object SpellNumber
+    inner(0, 0L, // Test values
+      ParSeq(-Long.MaxValue, -1000000000, 12, 13, 19, 20, 21, 112, 1001, 1012, 1013,
+        Long.MaxValue - 1, Long.MaxValue - 13, testVal1, testVal9)) ++
+      (for (z <- 0 to 69) yield BigInt(10).pow(z)) // powers of ten
+  }.seq.sorted.
+    foreach(num => println(f"""$num%+,80d -> ${longhand(v = num, showAnd = true)}"""))
+}  // object SpellNumber
